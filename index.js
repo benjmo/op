63ef61019f6c46bb1a681192111a59e0b4ee6ac8
Damon Ho <damon.ho@student.unsw.edu.au>
2017-09-13 22:31:52 +1000

    Code for canvas

---
+++ index.js
@@ -0,0 +1,29 @@
+var app = require('express')();
+var path = require('path');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+app.get('/draw/', function(req, res){
+    res.render('index',{drawing: true});
+}).get('/drawing.js', function(req,res) {
+    res.sendFile(path.join(__dirname, 'drawing.js'));
+});
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('draw', function(data) {
+        console.log(data);
+        io.emit('draw', data);
+    })
+});
+
+var port = process.env.PORT || 3000;
+
+http.listen(port, function(){
+    console.log('listening on *:'+port);
+});
