0b50a11e9333bae59d11f48bdce24238e202179a
Damon Ho <damon.ho@student.unsw.edu.au>
2017-09-15 18:25:56 +1000

    Added scrolling for messages

--- static/js/messenger.js
+++ static/js/messenger.js
@@ -1,10 +1,36 @@
+
+
+function scrollBottom(){
+    if(!preventNewScroll){ // if mouse is not over printer
+        $printer.stop().animate( {scrollTop: $printer[0].scrollHeight - printerH  }, 600); // SET SCROLLER TO BOTTOM
+    }
+}
+
 function initMessenger(messageDiv,inputForm,msg,socket) {
+    let printerH  = messageDiv.innerHeight(),
+    preventNewScroll = false;
+    let scrollBottom = () => {
+        if(!preventNewScroll){ // if mouse is not over printer
+            messageDiv.stop().animate( {scrollTop: messageDiv[0].scrollHeight - printerH  }, 600); // SET SCROLLER TO BOTTOM
+        }
+    };
+
+    messageDiv.hover(function( e ) {
+        preventNewScroll = e.type=='mouseenter' ? true : false ;
+        if(!preventNewScroll){ scrollBottom(); } // On mouseleave go to bottom
+    });
+
+    scrollBottom();
+
     inputForm.submit(()=> {
-        socket.emit('chat_message', msg.val());
-        msg.val('');
+        if (msg.val().trim() != '') {
+            socket.emit('chat_message', msg.val());
+            msg.val('');
+        }
         return false;
     });
     socket.on('chat_message',(msg) => {
         messageDiv.append($('<li>').text(msg));
+        scrollBottom();
     });
 }
