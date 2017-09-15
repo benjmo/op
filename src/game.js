153ea4cda27bab2c24fa33658be501704c37a573
Damon Ho <damon.ho@student.unsw.edu.au>
2017-09-16 00:16:18 +1000

    Added rooms/reworked JS

--- src/game.js
+++ src/game.js
@@ -6,58 +6,57 @@ let rooms = {};
 /*
  * Attempt to handle lobbies/rooms
  */
-const join_room = (io,socket,data) => {
-    console.log(data)
-    socket.join(data, () => {
-        if (!(data in rooms))
-            rooms[data] = [];
-        rooms[data].push(socket.id);
-        socket.broadcast.to(data).emit('chat_message','a new user has joined the room'); // broadcast to everyone in the room
-    });
+const join_room = (io, socket, data) => {
+  console.log(data)
+  socket.join(data, () => {
+    if (!(data in rooms))
+      rooms[data] = [];
+    rooms[data].push(socket.id);
+    socket.broadcast.to(data).emit('chat_message', 'a new user has joined the room'); // broadcast to everyone in the room
+  });
 };
 
 /**
  * Handle user drawing
  */
-const draw = (io,socket,data) => {
-    if (Object.keys(socket.rooms).length > 1) {
-        for (room of Object.keys(socket.rooms))
-            if (room !== socket.id)
-                socket.broadcast.to(room).emit('draw', data);
-    } else
-        io.emit('draw',data);
+const draw = (io, socket, data) => {
+  if (Object.keys(socket.rooms).length > 1) {
+    for (room of Object.keys(socket.rooms))
+      if (room !== socket.id)
+        socket.broadcast.to(room).emit('draw', data);
+  } else
+    io.emit('draw', data);
 
 };
 
 /**
  * Handle messaging
  */
-const chat_message = (io,socket,data) => {
-    if (data.match(/^!guess /)) {
-        let guess = data.replace(/^!guess /,"");
-        for (room of Object.keys(socket.rooms))
-            if (room !== socket.id)
-                io.to(room).emit('chat_message', "You Guessed ${guess}");
-    } else if (data.match(/^!join /)) {
-        let room = data.replace(/^!join /,"");
-        join_room(io,socket,room);
-    } else {
-        console.log(Object.keys(socket.rooms))
-        if (Object.keys(socket.rooms).length > 1) {
-            for (room of Object.keys(socket.rooms))
-                if (room !== socket.id) {
-                    console.log(room)
-                    io.to(room).emit('chat_message',data);
-                }
-        } else
-            io.emit('chat_message',data);
-    }
+const chat_message = (io, socket, data) => {
+  if (data.match(/^!guess /)) {
+    let guess = data.replace(/^!guess /, "");
+    for (room of Object.keys(socket.rooms))
+      if (room !== socket.id)
+        io.to(room).emit('chat_message', "You Guessed ${guess}");
+  } else if (data.match(/^!join /)) {
+    let room = data.replace(/^!join /, "");
+    join_room(io, socket, room);
+  } else {
+    console.log(Object.keys(socket.rooms))
+    if (Object.keys(socket.rooms).length > 1) {
+      for (room of Object.keys(socket.rooms))
+        if (room !== socket.id) {
+          io.to(room).emit('chat_message', data);
+        }
+    } else
+      io.emit('chat_message', data);
+  }
 };
 
-module.exports = function(io) {
-    return {
-        chat_message: chat_message.bind(this, io),
-        draw: draw.bind(this, io),
-        join_room: join_room.bind(this, io),
-    };
+module.exports = function (io) {
+  return {
+    chat_message: chat_message.bind(this, io),
+    draw: draw.bind(this, io),
+    join_room: join_room.bind(this, io),
+  };
 };
--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
 const express = require('express');
-const app     = express();
-const path    = require('path');
-const http    = require('http').Server(app);
-const io      = require('socket.io')(http);
-const game    = require('./game')(io);
+const app = express();
+const path = require('path');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const game = require('./game')(io);
 
 app.use(express.static('static'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
 /* Routes all go here */
-app.get('/', function(req, res){
-    res.render('index',);
+app.get('/', function (req, res) {
+  res.render('index',);
 });
-app.get('/play/:id', function(req, res){
-    res.render('index', {game: req.params.id, drawing: true});
+app.get('/play/:id', function (req, res) {
+  res.render('index', {game: req.params.id, drawing: true});
 });
-app.get('/draw/', function(req, res){
-    res.render('index',{drawing: true});
+app.get('/draw/', function (req, res) {
+  res.render('index', {drawing: true});
 });
-app.get('/message/', function(req, res) {
-   res.render('messages');
+app.get('/message/', function (req, res) {
+  res.render('messages');
 });
 
 /* top level socket.io stuff goes here */
-io.on('connection', function(socket) {
-    console.log('a user connected');
-    socket.on('join_room', (data) => {game.join_room(socket,data)})
-    socket.on('draw', (data) => game.draw(socket, data));
-    socket.on('chat_message',  (data) => game.chat_message(socket, data));
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  socket.on('join_room', (data) => {game.join_room(socket, data)});
+  socket.on('draw', (data) => game.draw(socket, data));
+  socket.on('chat_message', (data) => game.chat_message(socket, data));
 });
 
 const port = process.env.PORT || 3000;
-http.listen(port, function(){
-    console.log('listening on *:'+port);
+http.listen(port, function () {
+  console.log('listening on *:' + port);
 });
