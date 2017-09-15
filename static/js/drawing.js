e3992825e6fbbdab6dfc93771b9557913147432f
Damon Ho <damon.ho@student.unsw.edu.au>
2017-09-16 00:04:34 +1000

    Rearranged how the whiteboard and messenger are generated

--- static/js/drawing.js
+++ static/js/drawing.js
@@ -1,75 +1,91 @@
-function initialiseCanvas(drawingCanvas, socket, draw) {
-  let context = drawingCanvas[0].getContext('2d');
-  if (context) {
-    let clickX = [];
-    let clickY = [];
-    let clickDrag = [];
-    let paint;
+(($) => {
+    $.fn.pictWhiteboard = function(options) {
+        let opt = $.extend({},$.fn.pictWhiteboard.defaults,options);
+        this.each((it) => {
+            let context = this[it].getContext('2d');
+            if (context) {
+                let clickX = [];
+                let clickY = [];
+                let clickDrag = [];
+                let paint;
 
-    const addClick = function (x, y, dragging) {
-      clickX.push(x);
-      clickY.push(y);
-      clickDrag.push(dragging);
-    }
+                const addClick = function (x, y, dragging) {
+                    clickX.push(x);
+                    clickY.push(y);
+                    clickDrag.push(dragging);
+                }
 
-    const redraw = function() {
-      context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+                const redraw = function () {
+                    context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
 
-      context.strokeStyle = "#df4b26";
-      context.lineJoin = "round";
-      context.lineWidth = 5;
+                    context.strokeStyle = "#df4b26";
+                    context.lineJoin = "round";
+                    context.lineWidth = 5;
 
-      for (let i = 0; i < clickX.length; i++) {
-        context.beginPath();
-        if (clickDrag[i] && i) {
-          context.moveTo(clickX[i - 1], clickY[i - 1]);
-        } else {
-          context.moveTo(clickX[i] - 1, clickY[i]);
-        }
-        context.lineTo(clickX[i], clickY[i]);
-        context.closePath();
-        context.stroke();
-      }
-    }
+                    for (let i = 0; i < clickX.length; i++) {
+                        context.beginPath();
+                        if (clickDrag[i] && i) {
+                            context.moveTo(clickX[i - 1], clickY[i - 1]);
+                        } else {
+                            context.moveTo(clickX[i] - 1, clickY[i]);
+                        }
+                        context.lineTo(clickX[i], clickY[i]);
+                        context.closePath();
+                        context.stroke();
+                    }
+                }
 
-    if (draw) {
-      drawingCanvas.mousedown(function(e) {
-        const mouseX = e.pageX - this.offsetLeft;
-        const mouseY = e.pageY - this.offsetTop;
-        console.log("MD X:" + mouseX + " Y:" + mouseY)
-        paint = true;
-        addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-        redraw();
-        socket.emit('draw', {
-          x: mouseX,
-          y: mouseY,
-          drag: false
+                let offsetLeft = 0, offsetTop = 0, a = this[it];
+                while (a) {
+                    offsetLeft += a.offsetLeft;
+                    offsetTop += a.offsetTop;
+                    a = a.offsetParent;
+                }
+                if (opt.draw) {
+                    this.mousedown(function (e) {
+                        console.log(this.offsetLeft);
+                        const mouseX = e.pageX - offsetLeft;
+                        const mouseY = e.pageY - offsetTop;
+                        console.log("MD X:" + mouseX + " Y:" + mouseY)
+                        paint = true;
+                        addClick(e.pageX - offsetLeft, e.pageY - offsetTop);
+                        redraw();
+                        opt.socket.emit('draw', {
+                            x: mouseX,
+                            y: mouseY,
+                            drag: false
+                        });
+                    }).mousemove(function (e) {
+                        if (paint) {
+                            const mouseX = e.pageX - offsetLeft;
+                            const mouseY = e.pageY - offsetTop;
+                            console.log("MM X:" + mouseX + " Y:" + mouseY)
+                            addClick(e.pageX - offsetLeft, e.pageY - offsetTop, true);
+                            redraw();
+                            opt.socket.emit('draw', {
+                                x: mouseX,
+                                y: mouseY,
+                                drag: true
+                            });
+                        }
+                    }).mouseleave(function (e) {
+                        console.log("ML")
+                        paint = false;
+                    }).mouseup(function (e) {
+                        console.log("MU")
+                        paint = false;
+                    });
+                }
+                opt.socket.on('draw', function (data) {
+                    addClick(data.x, data.y, data.drag);
+                    redraw();
+                });
+            }
         });
-      }).mousemove(function(e) {
-        if (paint) {
-          const mouseX = e.pageX - this.offsetLeft;
-          const mouseY = e.pageY - this.offsetTop;
-          console.log("MM X:" + mouseX + " Y:" + mouseY)
-          addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
-          redraw();
-          socket.emit('draw', {
-            x: mouseX,
-            y: mouseY,
-            drag: true
-          });
-        }
-      }).mouseleave(function(e) {
-        console.log("ML")
-        paint = false;
-      }).mouseup(function(e) {
-        console.log("MU")
-        paint = false;
-      });
-    } else {
-      socket.on('draw', function(data){
-        addClick(data.x,data.y,data.drag);
-        redraw();
-      });
-    }
-  }
-}
+    };
+
+    $.fn.pictWhiteboard.defaults = {
+        socket: io(),
+        draw: false
+    };
+})(jQuery);
--- static/js/messenger.js
+++ static/js/messenger.js
@@ -1,36 +1,50 @@
-
-
-function scrollBottom(){
-    if(!preventNewScroll){ // if mouse is not over printer
-        $printer.stop().animate( {scrollTop: $printer[0].scrollHeight - printerH  }, 600); // SET SCROLLER TO BOTTOM
-    }
-}
-
-function initMessenger(messageDiv,inputForm,msg,socket) {
-    let printerH  = messageDiv.innerHeight(),
-    preventNewScroll = false;
-    let scrollBottom = () => {
-        if(!preventNewScroll){ // if mouse is not over printer
-            messageDiv.stop().animate( {scrollTop: messageDiv[0].scrollHeight - printerH  }, 600); // SET SCROLLER TO BOTTOM
+(($) => {
+    function scrollBottom() {
+        if (!preventNewScroll) { // if mouse is not over printer
+            $printer.stop().animate({scrollTop: $printer[0].scrollHeight - printerH}, 600); // SET SCROLLER TO BOTTOM
         }
+    }
+    $.fn.pictMessenger = function(options) {
+        let opts = $.extend({},$.fn.pictMessenger.defaults,options);
+        return this.each(() => {
+            let inputForm = $('<form>').append($('<input>')).append($('<button>').text('Send'));
+            let messageDiv = $('<ul>');
+            this.append(messageDiv).append(inputForm);
+            messageDiv.height($(window).height() * opts.height / 100 - inputForm.height());
+            let printerH = messageDiv.innerHeight(),
+                preventNewScroll = false;
+            let scrollBottom = () => {
+                if (!preventNewScroll) { // if mouse is not over printer
+                    messageDiv.stop().animate({scrollTop: messageDiv[0].scrollHeight - printerH}, 250); // SET SCROLLER TO BOTTOM
+                }
+            };
+
+            messageDiv.hover(function (e) {
+                preventNewScroll = e.type == 'mouseenter' ? true : false;
+                if (!preventNewScroll) {
+                    scrollBottom();
+                } // On mouseleave go to bottom
+            });
+
+            scrollBottom();
+
+            inputForm.submit(() => {
+                let msg = $(this).find('input');
+                if (msg.val().trim() != '') {
+                    opts.socket.emit('chat_message', msg.val());
+                    msg.val('');
+                }
+                return false;
+            });
+            opts.socket.on('chat_message', (msg) => {
+                messageDiv.append($('<li>').text(msg));
+                scrollBottom();
+            });
+        });
     };
 
-    messageDiv.hover(function( e ) {
-        preventNewScroll = e.type=='mouseenter' ? true : false ;
-        if(!preventNewScroll){ scrollBottom(); } // On mouseleave go to bottom
-    });
-
-    scrollBottom();
-
-    inputForm.submit(()=> {
-        if (msg.val().trim() != '') {
-            socket.emit('chat_message', msg.val());
-            msg.val('');
-        }
-        return false;
-    });
-    socket.on('chat_message',(msg) => {
-        messageDiv.append($('<li>').text(msg));
-        scrollBottom();
-    });
-}
+    $.fn.pictMessenger.defaults = {
+        socket: io(),
+        height: 33
+    };
+})(jQuery);
