03b85e442bea54dc80062f36e8b1cc841327b491
Damon Ho <damon.ho@student.unsw.edu.au>
2017-09-19 16:35:40 +1000

    Added responsive canvas, added a clear() function declaration

--- static/js/drawing.js
+++ static/js/drawing.js
@@ -13,7 +13,7 @@
    * clicks stores all recorded mouse movements
    * options stores the socket and draw permissions
    */
-  function Plugin(element,options) {
+  function Whiteboard(element,options) {
     this.element = element;
     this.options = $.extend({}, defaults, options);
     this._defaults = defaults;
@@ -25,70 +25,45 @@
   /*
    * Functions callable using object.pictWhiteboard().functionName()
    */
-  Plugin.prototype = {
+  Whiteboard.prototype = {
     /*
      * Setup the whiteboard, and drawing mechanism
      */
     _init: function () {
       let context = this.element.getContext('2d');
       let plugin = this;
+      $(this.element).attr("width", $(this.element).outerWidth()).attr("height", this.element.width);
       if (context) {
         let paint;
-        /*
-         * Adds a mouse movement to the clicks array
-         */
-        const addClick = (x, y, dragging) => {
-          this.clicks.push({
-            x, y, dragging
-          })
-        };
-
-        /*
-         * Clears the canvas and redraws all recorded mouse movements
-         */
-        const redraw = () => {
-          context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
-          context.lineJoin = "round";
-          context.lineWidth = 5;
-          context.strokeStyle = "#000000";
-          for (let i = 0; i < this.clicks.length; i++) {
-            context.beginPath();
-            if (this.clicks[i].dragging && i) {
-              context.moveTo(this.clicks[i - 1].x, this.clicks[i - 1].y);
-            } else {
-              context.moveTo(this.clicks[i].x - 1, this.clicks[i].y);
-            }
-            context.lineTo(this.clicks[i].x, this.clicks[i].y);
-            context.closePath();
-            context.stroke();
-          }
-        };
 
         let offsetLeft = 0, offsetTop = 0; // relative to whole page
-
+        let height = 0, width = 0;
         /*
          * Listeners for mouse events
          */
         if (this.options.draw) {
           $(this.element).mousedown(function (e) {
+            $(plugin.element).attr("width", $(plugin.element).outerWidth()).attr("height", plugin.element.width);
             /*
              * Calculates offset of canvas relative to whole page
              * Check every mousedown in case of resize
              */
             let a = this;
-            offsetLeft = 0; offsetTop = 0;
+            let height = this.height, width = this.width;
+            offsetLeft = 0;
+            offsetTop = 0;
             while (a) {
               offsetLeft += a.offsetLeft;
               offsetTop += a.offsetTop;
               a = a.offsetParent;
             }
 
-            const mouseX = e.pageX - offsetLeft;
-            const mouseY = e.pageY - offsetTop;
+            const mouseX = (e.pageX - offsetLeft) / height;
+            const mouseY = (e.pageY - offsetTop) / width;
             // console.log("MD X:" + mouseX + " Y:" + mouseY)
             paint = true;
-            addClick(e.pageX - offsetLeft, e.pageY - offsetTop);
-            redraw();
+            plugin.addClick(mouseX, mouseY);
+            plugin.redraw();
             plugin.options.socket.emit('draw', {
               x: mouseX,
               y: mouseY,
@@ -96,11 +71,12 @@
             });
           }).mousemove(function (e) {
             if (paint) {
-              const mouseX = e.pageX - offsetLeft;
-              const mouseY = e.pageY - offsetTop;
+              let height = this.height, width = this.width;
+              const mouseX = (e.pageX - offsetLeft) / height;
+              const mouseY = (e.pageY - offsetTop) / width;
               // console.log("MM X:" + mouseX + " Y:" + mouseY)
-              addClick(e.pageX - offsetLeft, e.pageY - offsetTop, true);
-              redraw();
+              plugin.addClick(mouseX, mouseY, true);
+              plugin.redraw();
               plugin.options.socket.emit('draw', {
                 x: mouseX,
                 y: mouseY,
@@ -120,11 +96,48 @@
          * Listener for draw's from other users
          */
         this.options.socket.on('draw', function (data) {
-          addClick(data.x, data.y, data.drag);
-          redraw();
+          plugin.addClick(data.x, data.y, data.drag);
+          plugin.redraw();
         });
       }
-    }
+    },
+
+    /*
+     * Adds a mouse movement to the clicks array
+     */
+    addClick: function (x, y, dragging) {
+      this.clicks.push({
+        x, y, dragging
+      })
+    },
+
+    /*
+     * Clears the canvas and redraws all recorded mouse movements
+     */
+    redraw: function () {
+      let context = this.element.getContext('2d');
+      context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+      context.lineJoin = "round";
+      context.lineWidth = 5;
+      context.strokeStyle = "#000000";
+      let height = this.element.height, width = this.element.width;
+      for (let i = 0; i < this.clicks.length; i++) {
+        context.beginPath();
+        if (this.clicks[i].dragging && i) {
+          context.moveTo(this.clicks[i - 1].x * width, this.clicks[i - 1].y * height);
+        } else {
+          context.moveTo(this.clicks[i].x * width - 1, this.clicks[i].y * height);
+        }
+        context.lineTo(this.clicks[i].x * width, this.clicks[i].y * height);
+        context.closePath();
+        context.stroke();
+      }
+    },
+
+    /*
+     * TODO: Implement clear Function
+     */
+    clear: function() {}
   };
 
   /*
@@ -135,7 +148,7 @@
     this.each(function () {
       if (!$.data(this, "plugin_" + pluginName)) {
         $.data(this, "plugin_" + pluginName,
-          new Plugin(this, options));
+          new Whiteboard(this, options));
       }
       ret = $.data(this, "plugin_" + pluginName)
     });
