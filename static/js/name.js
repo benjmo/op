77dac2a0de746cdfb10c2061d0813a6d174b8d31
Damon Ho <damon.ho@student.unsw.edu.au>
2017-09-27 19:40:44 +1000

    Fixd name being null if prompt is cancelled

--- static/js/name.js
+++ static/js/name.js
@@ -4,7 +4,11 @@
 get_username = function (socket) {
   // Get username from user and check if it is unique
   let unique = false;
-  let username = prompt("Please enter your name:", "anon"+Math.trunc(Math.random()*10000));
+  let defaultName = "anon"+Math.trunc(Math.random()*10000);
+  let username = prompt("Please enter your name:", defaultName);
+  //handle if cancelled
+  if (!username)
+    username = defaultName;
   socket.emit('nameMessage', username);
 
   socket.on('nameMessage', (unique) => {
