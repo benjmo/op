f95390bd625fae171303d106bada4bde2ac38ac9
benjamintimbrell <benjamintimbrell@gmail.com>
2017-10-22 23:25:46 +1100

    Improve name validation

--- static/js/name.js
+++ static/js/name.js
@@ -3,7 +3,6 @@
  */
 get_username = function (socket) {
   // Get username from user and check if it is unique
-  let unique = false;
   let defaultName = "anon"+Math.trunc(Math.random()*10000);
   let username = prompt("Please enter your name:", defaultName);
   //handle if cancelled
@@ -12,15 +11,12 @@ get_username = function (socket) {
   socket.emit('nameMessage', username);
 
   socket.on('nameMessage', (unique) => {
-    if (unique)
-      unique = true;
-    else {
+    if (!unique) {
       username = prompt("That name is taken! Please enter your name:", "anon"+Math.trunc(Math.random()*10000));
+      //handle if cancelled
+      if (!username)
+        username = defaultName;
       socket.emit('nameMessage', username);
     }
   });
-  // Loop until username is unique
-  // while (!unique) {
-  // }
-
 };
